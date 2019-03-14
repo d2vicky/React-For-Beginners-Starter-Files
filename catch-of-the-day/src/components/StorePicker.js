2c26016ef3e3c4f1401d8831a0bfe4044aeb2228
d2vicky <vicky_beauty@qq.com>
2019-03-13 17:50:01 -0700

    11 Bind function with constructor

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	constructor() {
+		super();
+		this.goToStore = this.goToStore.bind(this);
+	}
+
+	goToStore(event) {
+		// 1. Prevent the form to submitting immediately
+		event.preventDefault();
+
+		// 2. Get the text from the input
+		console.log(this);  // trying to see if this function can get the correct "this"
+		
+		// 3. Change the path/page to /store/:storeID
+
+	}
+
 	render() {
 		return (
 			<React.Fragment>
 				{ /* This is a Store Picker*/}
-				<form className="store-selector">
+				<form className="store-selector" onSubmit={this.goToStore}>
 					<h2>Please Select A Store</h2>
 					<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
 					<button type="submit">Visit Store -></button>
