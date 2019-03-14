94b462212e20376224f23f086e43326c84ab9a98
d2vicky <vicky_beauty@qq.com>
2019-03-13 17:54:23 -0700

    11 Create ref to the input so that we can have its text

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	myInput = React.createRef();
 
 	goToStore = event => {
 		// 1. Prevent the form to submitting immediately
 		event.preventDefault();
 
 		// 2. Get the text from the input
-		console.log(this);  // trying to see if this function can get the correct "this"
+		console.log(this.myInput);  // trying to see if this function can get the correct "this"
 		
 		// 3. Change the path/page to /store/:storeID
 
@@ -20,7 +21,13 @@ class StorePicker extends React.Component {
 				{ /* This is a Store Picker*/}
 				<form className="store-selector" onSubmit={this.goToStore}>
 					<h2>Please Select A Store</h2>
-					<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+					<input
+						type="text"
+						ref={this.myInput}
+						required
+						placeholder="Store Name"
+						defaultValue={getFunName()}
+					/>
 					<button type="submit">Visit Store -></button>
 				</form>
 			</React.Fragment>
